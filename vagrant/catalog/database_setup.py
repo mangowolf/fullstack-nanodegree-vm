8e3bdd3cc9a877b994a9e88d9315ea0d7023e8fb
Jimmy Nguyen <jimmy@jimmys-mbp.gateway.sonic.net>
2017-03-14 22:05:44 -0700

    Import sqlalchemy package in database_setup.py

--- vagrant/catalog/database_setup.py
+++ vagrant/catalog/database_setup.py
@@ -1,1 +1,5 @@
 #Database Setup
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
