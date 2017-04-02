965152fe9f3a2c19d84b46cc16e2f3bb36a42215
Jimmy Nguyen <jimmy@jimmys-mbp.gateway.sonic.net>
2017-04-01 22:10:06 -0700

    Google sign in feature added.

--- vagrant/catalog/itemCatalogProject.py
+++ vagrant/catalog/itemCatalogProject.py
@@ -5,9 +5,9 @@ from database_setup import Base, Category, Item, User
 from flask import session as login_session
 import random
 import string
-#from oauth2client.client import flow_from_clientsecrets
-#from oauth2client.client import FlowExchangeError
-#import httplib2
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
+import httplib2
 import json
 from flask import make_response
 import requests
@@ -35,18 +35,156 @@ def showLogin():
 
 @app.route('/gconnect', methods=['POST'])
 def gconnect():
-	#Validate state token
+	# Validate state token
 	if request.args.get('state') != login_session['state']:
 		response = make_response(json.dumps('Invalid state parameter.'), 401)
 		response.headers['Content-Type'] = 'application/json'
 		return response
-	#Obtain authorization code
+	# Obtain authorization code
 	code = request.data
+
+	try:
+		# Upgrade the authorization code into a credentials object
+		oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
+		oauth_flow.redirect_uri = 'postmessage'
+		credentials = oauth_flow.step2_exchange(code)
+	except FlowExchangeError:
+		response = make_response(
+			json.dumps('Failed to upgrade the authorization code.'), 401)
+		response.headers['Content-Type'] = 'application/json'
+		return response
+
+	# Check that the access token is valid.
+	access_token = credentials.access_token
+	url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s' % access_token)
+	h = httplib2.Http()
+	result = json.loads(h.request(url, 'GET')[1])
+
+	# If there was an error in the access token info, abort.
+	if result.get('error') is not None:
+		response = make_response(json.dumps(result.get('error')), 500)
+		response.headers['Content-Type'] = 'application/json'
+		return response
+
+	# Verify that the access token is used for the intended user.
+	gplus_id = credentials.id_token['sub']
+	if result['user_id'] != gplus_id:
+		response = make_response(
+			json.dumps("Token's user ID doesn't match given user ID."), 401)
+		response.headers['Content-Type'] = 'application/json'
+		return response
+
+	# Verify that the access token is avlid for this app.
+	if result['issued_to'] != CLIENT_ID:
+		response = make_response(
+			json.dumps("Token's Client ID does not match app's."), 401)
+		print "Token's client ID does not match app's."
+		response.headers['Content-Type'] = 'application/json'
+		return response
+
+	stored_credentials = login_session.get('credentials')
+	stored_gplus_id = login_session.get('gplus_id')
+	if stored_credentials is not None and gplus_id == stored_gplus_id:
+		response = make_response(json.dumps('Current user is already connected.'), 200)
+		response.headers['Content-Type'] = 'application/json'
+		return response
+
+	# Store the access token in the session for later use.
+	login_session['credentials'] = credentials
+	login_session['access_token'] = credentials.access_token
+	login_session['gplus_id'] = gplus_id
+
+	# Get user info
+	userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+	params = {'access_token': credentials.access_token, 'alt': 'json'}
+	answer = requests.get(userinfo_url, params=params)
+
+	data = answer.json()
+
+	login_session['username'] = data['name']
+	login_session['picture'] = data['picture']
+	login_session['email'] = data['email']
+
+	# Add provider to login session
+	login_session['provider'] = 'google'
+
+	# see if user exists, if it doesn't make a new one
+	user_id = getUserID(data["email"])
+	if not user_id:
+		user_id = createUser(login_session)
+	login_session['user-id'] = user_id
+
+	output = ''
+	output += '<h1>Welcome, '
+	output += login_session['username']
+	output += '!</h1>'
+	output += 'img src="'
+	output += login_session['picture']
+	output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+	flash("you are now logged in as %s" % login_session['username'])
+	print "done!"
+	return output
 
 ## Session Commit Convenience Function
 def commitSession(argument):
 	session.add(argument)
 	session.commit()
+
+# user Helper Functions
+
+def createUser(login_session):
+	newUser = User(name=login_session['username'], email=login_session['email'], picture=login_session['picture'])
+	commitSession(newUser)
+	user = session.query(User).filter_by(email=login_session['email']).one()
+	return user.id
+
+def getUserInfo(user_id):
+	user = session.query(User).filter_by(id=user_id).one()
+	return user
+
+def getUserID(email):
+	try:
+		user = session.query(User).filter_by(email=email).one()
+		return user.id
+	except:
+		return None
+
+# DISCONNECT - Revoke a current user's token and reset their login_session
+
+
+@app.route('/gdisconnect')
+def gdisconnect():
+    # Only disconnect a connected user.
+    credentials = login_session.get('credentials')
+    if credentials is None:
+        response = make_response(
+            json.dumps('Current user not connected.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    access_token = credentials.access_token
+    url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % access_token
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[0]
+    if result['status'] == '200':
+        #Reset the user's session
+        del login_session['credentials']
+        del login_session['gplus_id']
+        del login_session['access_token']
+        del login_session['username']
+        del login_session['email']
+        del login_session['picture']
+
+        response = make_response(
+          json.dumps('Successfully disconnected'), 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    else:
+        # For whatever reason, the given token was invalid.
+        response = make_response(
+            json.dumps('Failed to revoke token for given user.'), 400)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
 
 # Show all categories
 @app.route('/')
